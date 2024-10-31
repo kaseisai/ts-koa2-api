bbb567c335f23f075022c7038ea6671a960b91a2
He ShengZai <kaseisai@163.com>
2024-10-31 22:37:30 +0800

    修复初始化项目启动失败的一些报错

--- src/common/utils/guid.ts
+++ src/common/utils/guid.ts
@@ -1,13 +1,13 @@
-import uuid = require('node-uuid');
+import uuid from 'node-uuid';
 
 export class Guid {
-    static GetGuid(length?: number): string {
-        const buffer = new Array(32);
-        const r = uuid.v4(null, buffer, 16);
-        const rr = bytesToUuid(r, 16);
+  static GetGuid(length?: number): string {
+    const buffer = new Array(32);
+    const r = uuid.v4(null, buffer, 16);
+    const rr = bytesToUuid(r, 16);
 
-        return rr.replace(/-/g, '').slice(0, length || 32);
-    }
+    return rr.replace(/-/g, '').slice(0, length || 32);
+  }
 }
 
 /**
@@ -16,15 +16,15 @@ export class Guid {
  */
 const byteToHex = [];
 for (let i = 0; i < 256; ++i) {
-    byteToHex[i] = (i + 0x100).toString(16).substr(1);
+  byteToHex[i] = (i + 0x100).toString(16).substr(1);
 }
 
 function bytesToUuid(buf, offset) {
-    let i = offset || 0;
-    const bth = byteToHex;
-    return `${bth[buf[i++]] + bth[buf[i++]] + bth[buf[i++]] + bth[buf[i++]]}-${bth[buf[i++]]}${bth[buf[i++]]}-${
-        bth[buf[i++]]
-    }${bth[buf[i++]]}-${bth[buf[i++]]}${bth[buf[i++]]}-${bth[buf[i++]]}${bth[buf[i++]]}${bth[buf[i++]]}${
-        bth[buf[i++]]
-    }${bth[buf[i++]]}${bth[buf[i++]]}`;
+  let i = offset || 0;
+  const bth = byteToHex;
+  return `${bth[buf[i++]] + bth[buf[i++]] + bth[buf[i++]] + bth[buf[i++]]}-${bth[buf[i++]]}${bth[buf[i++]]}-${
+    bth[buf[i++]]
+  }${bth[buf[i++]]}-${bth[buf[i++]]}${bth[buf[i++]]}-${bth[buf[i++]]}${bth[buf[i++]]}${bth[buf[i++]]}${bth[buf[i++]]}${
+    bth[buf[i++]]
+  }${bth[buf[i++]]}`;
 }
